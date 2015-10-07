267194b1ebfe37fc29fde49f73b4377f68094c9c
tigershen23 <tigershen23@gmail.com>
2015-10-07 13:30:53 -0700

    Add and render house image on SearchPage

--- jsx/SearchPage.js
+++ jsx/SearchPage.js
@@ -37,6 +37,7 @@ class SearchPage extends Component {
           underlayColor='#99d9f4'>
           <Text style={styles.buttonText}>Location</Text>
         </TouchableHighlight>
+        <Image source={require('image!house')} style={styles.image} />
       </View>
     );
   }
@@ -86,7 +87,11 @@ var styles = StyleSheet.create({
     borderColor: '#48BBEC',
     borderRadius: 8,
     color: '#48BBEC'
-  }
+  },
+  image: {
+    width: 217,
+    height: 138
+  },
 });
 
 module.exports = SearchPage;
