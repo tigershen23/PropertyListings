7d9dafff91341116df56f1a5ecb91d56bf3ad065
tigershen23 <tigershen23@gmail.com>
2015-10-07 13:26:12 -0700

    Add dummy search form to SearchPage component

--- jsx/SearchPage.js
+++ jsx/SearchPage.js
@@ -22,6 +22,21 @@ class SearchPage extends Component {
         <Text style={styles.description}>
           Search by name, postal code, or for properties near you.
         </Text>
+        <View style={styles.flowRight}>
+          <TextInput
+            style={styles.searchInput}
+            placeholder='...' />
+          <TouchableHighlight
+            style={styles.button}
+            underlayColor='#99d9f4'>
+            <Text style={styles.buttonText}>Go</Text>
+          </TouchableHighlight>
+        </View>
+        <TouchableHighlight
+          style={styles.button}
+          underlayColor='#99d9f4'>
+          <Text style={styles.buttonText}>Location</Text>
+        </TouchableHighlight>
       </View>
     );
   }
@@ -39,6 +54,39 @@ var styles = StyleSheet.create({
     marginTop: 65,
     alignItems: 'center'
   },
+  flowRight: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    alignSelf: 'stretch'
+  },
+  buttonText: {
+    fontSize: 18,
+    color: 'white',
+    alignSelf: 'center'
+  },
+  button: {
+    height: 36,
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: '#48BBEC',
+    borderColor: '#48BBEC',
+    borderWidth: 1,
+    borderRadius: 8,
+    marginBottom: 10,
+    alignSelf: 'stretch',
+    justifyContent: 'center'
+  },
+  searchInput: {
+    height: 36,
+    padding: 4,
+    marginRight: 5,
+    flex: 4,
+    fontSize: 18,
+    borderWidth: 1,
+    borderColor: '#48BBEC',
+    borderRadius: 8,
+    color: '#48BBEC'
+  }
 });
 
 module.exports = SearchPage;
